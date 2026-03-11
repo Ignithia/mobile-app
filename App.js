276caa738f95ddf61e04c9b2b8ad59e95ace861b
Ignithia <75039029+Ignithia@users.noreply.github.com>
2026-03-11 17:44:53 +0100

    Enhance ProductCard and App components with improved layout and functionality

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import ProductCard from "./components/ProductCard";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      <Text style={styles.heading}>Our offer</Text>
+      <ScrollView style={styles.container} contentContainerStyle={styles.list}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#000",
+  },
+  heading: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "700",
+    textAlign: "center",
+    marginTop: 64,
+    marginBottom: 12,
+  },
+  list: {
+    paddingHorizontal: 12,
+    paddingBottom: 24,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
   },
 });
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,38 +1,98 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  Button,
+  Switch,
+  TextInput,
+} from "react-native";
 
 const ProductCard = ({ product }) => {
   return (
     <View style={styles.card}>
-      <Image source={require("../images/test.jpeg")} style={styles.image} />
-      <Text style={styles.title}>Le frog</Text>
-      <Text style={styles.description}>A frog</Text>
+      <Image source={require("../images/imdages.jpeg")} style={styles.image} />
+      <Text style={styles.title}>Batman Skateboard</Text>
+      <Text style={styles.description}>Skateboard met batman logo</Text>
+      <Text style={styles.price}>$29.99</Text>
+      <Button
+        style={styles.button}
+        OnPress={() => alert("You bought a skateboard")}
+        title="Buy Now"
+      ></Button>
+      <Pressable
+        style={styles.button}
+        onPress={() => alert("Viewing details for Batman Skateboard")}
+      >
+        <Text style={styles.buttonText}>View Details</Text>
+      </Pressable>
+      <Text style={{ color: "#fff" }}>Add to Wishlist</Text>
+      <Switch
+        style={styles.switch}
+        onValueChange={() => alert("Added to wishlist")}
+        value={false}
+        title="Add to Wishlist"
+      ></Switch>
+      <TextInput placeholder="Enter quantity" style={styles.input} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   card: {
-    width: 300,
+    width: "48%",
     padding: 16,
-    backgroundColor: "#fff",
-    borderRadius: 8,
+    backgroundColor: "#1c887f",
+    borderRadius: 12,
     marginBottom: 16,
   },
   image: {
     width: "100%",
     height: 150,
-    borderRadius: 8,
+    borderRadius: 10,
   },
   title: {
     fontSize: 18,
     fontWeight: "bold",
     marginTop: 8,
+    color: "#fff",
   },
   description: {
     fontSize: 14,
-    color: "#555",
+    color: "#d0d0d0",
     marginTop: 4,
+  },
+  price: {
+    fontSize: 16,
+    color: "#fff",
+    marginTop: 8,
+    fontWeight: "700",
+  },
+  button: {
+    marginTop: 12,
+    backgroundColor: "#ff0000",
+    color: "#fff",
+    borderRadius: 10,
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  switch: {
+    backgroundColor: "#04fb08",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "700",
+  },
+  input: {
+    marginTop: 12,
+    backgroundColor: "#333",
+    borderColor: "#555",
+    borderWidth: 1,
+    color: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
   },
 });
 
