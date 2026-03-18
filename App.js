46f502ce82717dd4b38eb97db5706d60c5f6390d
Ignithia <75039029+Ignithia@users.noreply.github.com>
2026-03-18 17:05:16 +0100

    Refactor navigation structure to use Tab.Navigator; add BlogCard and ProductDetail components; implement BlogsScreen with blog listings

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import {} from "react-native";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -9,14 +6,14 @@ import HomeScreen from "./screens/HomeScreen.js";
 import ProductDetail from "./screens/ProductDetail.js";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={ProductDetail} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
